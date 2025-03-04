87f09eae37f54b8e3f81bb5aeb15e81c3100e2e8
geekftz <33424860+geekftz@users.noreply.github.com>
2025-03-04 13:57:53 +0800

    feat: hash route and history route

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,77 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+
+// HASH ROUTER
+// 定义 Router  
+// class Router {  
+//     constructor () {  
+//         this.routes = {}; // 存放路由path及callback  
+//         this.currentUrl = '';  
+          
+//         // 监听路由change调用相对应的路由回调  
+//         window.addEventListener('load', this.refresh, false);  
+//         window.addEventListener('hashchange', this.refresh, false);  
+//     }  
+      
+//     route(path, callback){  
+//         this.routes[path] = callback;  
+//     }  
+      
+//     push(path) {  
+//       // 更新 URL 的 hash，会自动触发 hashchange 事件
+//       location.hash = path;
+//       this.routes[path] && this.routes[path]() 
+//     }  
+// }  
+  
+// // 使用 router  
+// window.miniRouter = new Router();  
+// miniRouter.route('/', () => console.log('page1'))  
+// miniRouter.route('/page2', () => console.log('page2'))  
+  
+// // miniRouter.push('/') // page1  
+// miniRouter.push('/page2') // page2  
+
+
+// HISTORY ROUTER
+// 定义 Router  
+class Router {  
+    constructor () {  
+        this.routes = {};  
+        this.listerPopState()  
+    }  
+      
+    init(path) {  
+        history.replaceState({path: path}, null, path);  
+        this.routes[path] && this.routes[path]();  
+    }  
+      
+    route(path, callback){  
+        this.routes[path] = callback;  
+    }  
+      
+    push(path) {  
+        history.pushState({path: path}, null, path);  
+        this.routes[path] && this.routes[path]();  
+    }  
+      
+    listerPopState () {  
+        window.addEventListener('popstate' , e => {  
+            const path = e.state && e.state.path;  
+            this.routers[path] && this.routers[path]()  
+        })  
+    }  
+}  
+  
+// 使用 Router  
+  
+window.miniRouter = new Router();  
+miniRouter.route('/', ()=> console.log('page1'))  
+miniRouter.route('/page2', ()=> console.log('page2'))  
+  
+// 跳转  
+// miniRouter.push('/page2')  // page2  
+
 </script>
 
 <template>
